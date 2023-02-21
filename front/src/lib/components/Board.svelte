e30480f8a4b02009052c7974816fd522668d51b7
unknown <pierre-alexis.hetic.net>
2023-02-21 15:23:08 +0100

    adding bg

---
+++ front/src/lib/components/Board.svelte
@@ -0,0 +1,24 @@
+<script>
+	import board_bg from '$static/bg_board.jpg';
+</script>
+<section>
+    <div class="background">
+        <img src="{board_bg}" alt="background" />
+    </div>
+</section>
+
+<style lang="scss">
+    .background {
+        position: absolute;
+        height: 100vh;
+        width: 100vw;
+        top: 0;
+        left: 0;
+        z-index: -100;
+    }
+    .background img {
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
