5565e8c6c4cd5c0dcd6740d61fbe3394868ef3b4
mathieu.parinet <mathieu.parinet@scub.net>
2023-02-22 13:16:43 +0100

    add socket

--- back/server.js
+++ back/server.js
@@ -4,37 +4,77 @@ import { Server } from "socket.io";
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"],
     },
 });
 
 io.on("connection", (socket) => {
-    socket.on("join_room", (room, cb) => {
+    socket.on("join_room", (room, nPlayer, callback) => {
         console.log("Connected");
         socket.join(room);
         socket.data.room = room;
-        cb({
-            value: `Connected`,
-        });
+
+        callback(nPlayer ?? io.sockets.adapter.rooms.get(room).size);
     });
 
     /**
-     * Sending to all clients in room except sender
+     * Sending to all clients in room
      * @param {string} name
      * @param {*} data
      */
-    const sender = (name, data) => {
-        socket.broadcast.to(socket.data.room).emit(name, data);
+    const sendAll = (name, data) => {
+        socket.to(socket.data.room).emit(name, data);
     };
 
-    socket.on("send_message", (data) => sender("get_message", data));
+    /**
+     * Sending to all client in room except sender
+     * @param {string} name
+     * @param {*} data
+     */
+    const sendAllExceptSender = (name, data) => {
+        socket.broadcast.to(socket.data.room).emit(name, data);
+    }
 
-    socket.on("send_arrivalPoint", (data) => sender("get_arrivalPoint", data));
+    /**
+    * Sending to on client in room
+    * @param {string} name
+     * @param {*} nPlayer
+    * @param {*} data
+     */
+    const sendTo = (name, nPlayer, data) => {
+        const socketId = io.sockets.adapter.rooms.get(socket.data.room)[nPlayer - 1];
+        socket.to(socket.data.room).to(socketId).emit(name, data);
+    }
 
-    socket.on("send_arrivalTime", (data) => sender("get_arrivalTime", data));
+    socket.on("send_pickCard", ({nPlayer,name}) => {
+        //TODO: traitement de la carte piochée et modification du JSON
 
-    socket.on("send_myData", (data) => sender("get_data", data));
+        //S'il n'y a plus de carte dans la pioche, la défausse devient la pioche et le signale à tous les joueurs
+
+        sendAllExceptSender("get_pickCard", null); //TODO: doit signaler qu'un joueur a pioché une carte
+        sendTo("get_pickCard", nPlayer, null); //TODO: doit renvoyer la carte piochée
+    });
+
+    socket.on("send_playCard", ({nPlayer,nPlayer2,card}) => {
+        //TODO: traitement de la carte jouée et modification du JSON
+
+        if (card.type === "distance") {
+            sendAll("get_distance", card.distance) //TODO: doit mettre à jour la distance
+        }
+
+        if(nPlayer2 === nPlayer){
+            sendAllExceptSender("get_playCard", null); //TODO: doit signaler que le joueur a joué une carte
+        } else{
+            sendAll("get_playCard", null); //TODO: doit signaler que le joueur a posé une carte chez l'adversaire
+        }
+    });
+
+    socket.on("send_discardCard", ({nPlayer,card}) => {
+        //TODO: traitement de la carte défaussée et modification du JSON
+
+        sendAll("get_discardCard", null); //TODO: doit signaler que le joueur a défaussé une carte
+    });
 });
 
 httpServer.listen(9999, () => {
